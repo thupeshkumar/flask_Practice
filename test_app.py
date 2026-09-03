13ad2a541831b241c7381d1fd2ca74b30a76de2c
thupeshkumar <59060523+thupeshkumar@users.noreply.github.com>
2026-09-03 17:02:36 +0530

    Update MongoDB URI in test fixture

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
-    app.config["MONGO_URI"] = "mongodb://root:password@localhost:27017/test_student_db"  # test DB
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/test_student_db"  
     client = app.test_client()
 
     # Setup: clear and create test data
